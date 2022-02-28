14bf454d6be709beed1f1157de5e2a5078c5d867
Azemard-Maxime <94792521+Azemard-Maxime@users.noreply.github.com>
2022-02-28 16:47:20 +0100

    Add files via upload

---
+++ Codes/MoteurJaune.ino
@@ -0,0 +1,17 @@
+// Moteur jaune
+
+int moteurJaune = 8;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(moteurJaune,OUTPUT);
+  Serial.begin(9600);
+  digitalWrite(moteurJaune,LOW);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+ 
+
+}
