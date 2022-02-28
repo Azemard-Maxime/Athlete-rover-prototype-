b10aec7d6b2106d36347aed814a41b64822bc358
Azemard-Maxime <94792521+Azemard-Maxime@users.noreply.github.com>
2022-02-28 16:48:32 +0100

    Add files via upload

---
+++ Codes/ORAL_servo___moteurs_Lego.ino
@@ -0,0 +1,94 @@
+#include <Servo.h>;
+Servo monServo;
+int positionServo = 2;
+
+
+// Moteur des roues
+int ENA = 9 ;
+int IN1 = 4 ;
+int IN2 = 5 ;
+
+// Moteur du bras articulé
+int ENB = 10 ;
+int IN3 = 6 ;
+int IN4 = 7 ;
+
+void setup() {
+  monServo.attach(2);
+  monServo.write(positionServo);
+  Serial.begin(9600);
+
+  
+  pinMode(ENA,OUTPUT);
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  
+  pinMode(ENB,OUTPUT);
+  pinMode(IN3,OUTPUT);
+  pinMode(IN4,OUTPUT);
+
+  //puissance des moteurs
+  digitalWrite(ENA,LOW);
+  digitalWrite(ENB,LOW);
+
+  //Sens des moteurs
+  // - Moteur roues
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2,HIGH);
+
+  // - Moteur bras
+  digitalWrite(IN3,LOW);
+  digitalWrite(IN4,HIGH);
+
+}
+
+void loop() {
+  
+  
+  
+    for (positionServo= 2 ; positionServo <= 100 ; positionServo++){
+      monServo.write(positionServo);
+      delay(40);
+      Serial.print(positionServo);
+    }
+  
+     for (positionServo= 100 ; positionServo >= 2 ; positionServo--){
+      monServo.write(positionServo);
+      delay(40);
+      Serial.print(positionServo);
+    }
+  
+
+  // roues en marche pendant X secondes
+  digitalWrite(ENA,255);
+  delay(4000);
+  digitalWrite(ENA,0);
+  Serial.print("roues");
+
+  // montée + arret + descente des roues
+  
+  digitalWrite(IN3,HIGH);
+  digitalWrite(IN4,LOW);
+  
+  digitalWrite(ENB,155);
+  delay(2000);
+  Serial.println("montée");
+  
+  digitalWrite(ENB,0);
+  delay(2000);
+  Serial.println("arret");
+  
+  digitalWrite(IN3,LOW);
+  digitalWrite(IN4,HIGH);
+  
+  digitalWrite(ENB,155);
+  delay(1500);
+  Serial.println("descente");
+  digitalWrite(ENB,0);
+  
+  digitalWrite(IN3,HIGH);
+  digitalWrite(IN4,LOW);
+  Serial.println("fin");
+  
+  
+}
